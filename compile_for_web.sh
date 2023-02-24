2c0d880cafcc3d5567215d215cdf811feb7743ea
Damjan <damjandjordjevic1994@gmail.com>
2023-02-24 11:43:34 +0100

    made it easier to compile for web

--- compile_for_web.sh
+++ compile_for_web.sh
@@ -1,1 +1,7 @@
-emcc -o game.html  -Os -Wall ../raylib/src/libraylib.a -I../raylib/src/ *.c -s USE_GLFW=3 -s ASYNCIFY  -DPLATFORM_WEB --preload-file assets
+#! /bin/sh
+
+# Make sure to have raylib recompiled for the WEB version.
+# Also make sure to have emcc toolchain in your path
+# And update the path to your raylib location
+RAYLIB_LOCATION="../raylib"
+emcc -o game.html  -Os -Wall $RAYLIB_LOCATION/src/libraylib.a -I$RAYLIB_LOCATION/src/ *.c -s USE_GLFW=3 -s ASYNCIFY  -DPLATFORM_WEB --preload-file assets
